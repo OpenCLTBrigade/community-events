566f44d83cce8fec24b0dbee8f3665695c2c8f52
Ryan Edge <chimon2000@users.noreply.github.com>
2019-02-28 08:14:36 +0000

    Added proxy and updated mock port

--- web/src/components/EventList/EventList.tsx
+++ web/src/components/EventList/EventList.tsx
@@ -1,63 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import { Heading } from "fannypack";
 import { EventCard } from "../EventCard/EventCard";
+import { useFetch } from "../../hooks/use-fetch";
 
-export class EventList extends Component {
-  state = {
-    events: createEvents()
-  };
+export function EventList() {
+  const [events, loading] = useFetch<Event[]>("/events");
+  if (loading) return <div>Loading...</div>;
 
-  render() {
-    return (
-      <React.Fragment>
-        <Heading>Events</Heading>
-        {this.state.events.map(event => (
-          <EventCard key={event.key} event={event} />
-        ))}
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <Heading>Events</Heading>
+
+      {events &&
+        events.map(event => <EventCard key={event.id} event={event} />)}
+    </React.Fragment>
+  );
 }
 
 export default EventList;
 
-const eventUrl =
-  "Make-Up Date - Special Ed Workshop Part 2: Measurable Goals &Types of Services";
-const time = "2:00pm";
-const location = "University City Regional";
-const locationRoom = "Community Room";
-const locationUrl = "https://www.cmlibrary.org/branch/university-city-regional";
-const learnMoreUrl =
-  "https://www.cmlibrary.org/event/96510-technology-tutors-1-1-support-session";
-
-const tags = [
-  ["Adults", "Older Adults", "writing", "Books/Literature", "Multicultural"]
-];
-
-const description =
-  "Please join us for a free 90-minute workshop: Writing & Sharing Your Personal Stories. Writers from Charlotte Lit will lead participants through a series of activities to discover, write, and tell stories of life in Charlotte.";
-
-const createEvents = (count: number = 20) => {
-  let events = [];
-  for (let index = 0; index < count; index++) {
-    events.push({
-      key: `event-${index}`,
-      title:
-        "Make-Up Date - Special Ed Workshop Part 2: Measurable Goals & Types of Services",
-      eventUrl,
-      description,
-      tags: tags[0],
-      date: "Sunday, January 20, 2019",
-      time,
-      locations: [
-        {
-          name: location,
-          locationRoom,
-          locationUrl
-        }
-      ],
-      learnMoreUrl
-    });
-  }
-  return events;
+type Event = {
+  id: number;
+  title: string;
+  description: string;
+  date: string;
+  tags: string[];
+  locations: Location[];
+  learnMoreUrl: string;
+};
+type Location = {
+  name: string;
+  locationRoom: string;
+  locationUrl: string;
 };
