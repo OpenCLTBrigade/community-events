4784667c4213d67a2317523f5771cb5e14c7f0ba
Cayde6 <stephaniementzer@gmail.com>
2019-03-04 16:56:06 -0500

    footer component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
-import { ThemeProvider } from "fannypack";
+import { ThemeProvider, Navigation } from "fannypack";
 import { Container } from "fannypack";
 import EventList from "./components/EventList/EventList";
+import Footer from "./components/Footer/Footer";
+
 
 class App extends Component {
   render() {
     return (
+      
       <ThemeProvider>
         <Container width="initial">
           <EventList />
         </Container>
+        <Footer />
       </ThemeProvider>
     );
   }
