ccd3b09faeb25c80bcf849463c426bebf1b64a31
Ryan <ryan.c.edge@outlook.com>
2019-03-09 01:14:08 -0500

    Apply prettier

--- web/src/components/EventList/EventList.tsx
+++ web/src/components/EventList/EventList.tsx
@@ -1,35 +1,37 @@
-import React from "react";
-import { Heading } from "fannypack";
-import { EventCard } from "../EventCard/EventCard";
-import { useFetch } from "../../hooks/use-fetch";
+import React from 'react'
+import { Heading } from 'fannypack'
+import { EventCard } from '../EventCard/EventCard'
+import { useFetch } from '../../hooks/use-fetch'
 
 export function EventList() {
-  const [events, loading] = useFetch<Event[]>("/api/events");
-  if (loading) return <div>Loading...</div>;
+    const [events, loading] = useFetch<Event[]>('/api/events')
+    if (loading) return <div>Loading...</div>
 
-  return (
-    <React.Fragment>
-      <Heading>Events</Heading>
+    return (
+        <React.Fragment>
+            <Heading>Events</Heading>
 
-      {events &&
-        events.map(event => <EventCard key={event.id} event={event} />)}
-    </React.Fragment>
-  );
+            {events &&
+                events.map((event) => (
+                    <EventCard key={event.id} event={event} />
+                ))}
+        </React.Fragment>
+    )
 }
 
-export default EventList;
+export default EventList
 
 type Event = {
-  id: number;
-  title: string;
-  description: string;
-  date: string;
-  tags: string[];
-  locations: Location[];
-  learnMoreUrl: string;
-};
+    id: number
+    title: string
+    description: string
+    date: string
+    tags: string[]
+    locations: Location[]
+    learnMoreUrl: string
+}
 type Location = {
-  name: string;
-  locationRoom: string;
-  locationUrl: string;
-};
+    name: string
+    locationRoom: string
+    locationUrl: string
+}
--- web/src/hooks/use-fetch.ts
+++ web/src/hooks/use-fetch.ts
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 function useFetch<T>(url: string): [T | undefined, boolean] {
-  const [data, setData] = useState<T | undefined>(undefined);
-  const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<T | undefined>(undefined)
+    const [loading, setLoading] = useState(true)
 
-  async function fetchUrl() {
-    const response = await fetch(url);
-    const json = await response.json();
-    console.log(json);
-    setData(json);
-    setLoading(false);
-  }
+    async function fetchUrl() {
+        const response = await fetch(url)
+        const json = await response.json()
+        console.log(json)
+        setData(json)
+        setLoading(false)
+    }
 
-  useEffect(() => {
-    fetchUrl();
-  }, []);
+    useEffect(() => {
+        fetchUrl()
+    }, [])
 
-  return [data, loading];
+    return [data, loading]
 }
-export { useFetch };
+export { useFetch }
